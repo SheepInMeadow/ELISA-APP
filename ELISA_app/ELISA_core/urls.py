aefa0568ee545090f47c201a54784bcefb599020
Mila <s1105428@student.hsleiden.nl>
2022-06-08 00:15:34 +0200

    Added template support :)

--- ELISA_app/ELISA_core/urls.py
+++ ELISA_app/ELISA_core/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', hi_mom),
+    path('', placeholder),
 ]
--- ELISA_app/ELISA_core/views.py
+++ ELISA_app/ELISA_core/views.py
@@ -2,5 +2,5 @@ from django.http import HttpResponse
 from django.shortcuts import  render, redirect
 
 # Create your views here.
-def hi_mom(request):
-    return HttpResponse("<h1>hi mom</h1>")
+def placeholder(request):
+    return render(request, "placeholder.html")
