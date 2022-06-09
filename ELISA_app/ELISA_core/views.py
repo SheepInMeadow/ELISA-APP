50929aaf14d22a282ad1f0b1188e7b068f62f5f8
david <s1114145@student.hsleiden.nl>
2022-06-09 12:29:29 +0200

    Input file van de ruwe data via bestand is nu werkende en wordt verwerkt in een bruikbaar format.

--- ELISA_app/ELISA_core/views.py
+++ ELISA_app/ELISA_core/views.py
@@ -6,7 +6,20 @@ def Home(request):
     return render(request, 'Home.html')
 
 def Input_data(request):
-    return render(request, 'Input_data.html')
+    if request.method == 'POST':
+        data = request.FILES['my_file'].readlines()
+        lines = list()
+        for i in data[1:-1]:
+            lines.append(i.strip().decode('utf-8'))
+        formatted_data = list()
+        for j in lines:
+            line = j.split('\t')
+            formatted_data.append(line)
+        formatted_data[0].insert(0, '#')
+        print(formatted_data)
+        return render(request, 'Input_data.html')
+    else:
+        return render(request, 'Input_data.html')
 
 def Plate_layout(request):
     return render(request, 'Plate_layout')
