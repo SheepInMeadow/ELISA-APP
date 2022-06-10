f2dd12f1d1a778ce35bf7921d4fdb6766d9ebab9
david <s1114145@student.hsleiden.nl>
2022-06-10 11:58:11 +0200

    Copy paste input now works properly with one table, the file input works with multiple files with either txt or xlsx formatting.

--- ELISA_app/ELISA_core/views.py
+++ ELISA_app/ELISA_core/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import  render, redirect
+import openpyxl
 
 # Create your views here.
 def Home(request):
@@ -7,19 +8,50 @@ def Home(request):
 
 def Input_data(request):
     if request.method == 'POST':
-        data = request.FILES['my_file'].readlines()
-        lines = list()
-        for i in data[1:-1]:
-            lines.append(i.strip().decode('utf-8'))
-        formatted_data = list()
-        for j in lines:
-            line = j.split('\t')
-            formatted_data.append(line)
-        formatted_data[0].insert(0, '#')
-        print(formatted_data)
+        if request.POST.get('text_submit'):
+            data = request.POST.get('text_input')
+            data = data.strip().split('\r\n')
+            formatted_data = formatting_txt(data, 1)
+        if request.POST.get('file_submit'):
+            for file in request.FILES.getlist('my_file'):
+                if str(file).split('.')[1] == 'txt':
+                    data = file.readlines()
+                    formatted_data = formatting_txt(data, 2)
+                elif str(file).split('.')[1] == 'xlsx':
+                    formatted_data = formatting_xlsx(file)
         return render(request, 'Input_data.html')
     else:
         return render(request, 'Input_data.html')
+
+def formatting_txt(data, counter):
+    lines = list()
+    for i in data[1:-1]:
+        if counter == 1:
+            lines.append(i.strip())
+        elif counter == 2:
+            lines.append(i.strip().decode('utf-8'))
+    formatted_data = list()
+    for j in lines:
+        line = j.split('\t')
+        formatted_data.append(line)
+    formatted_data[0].insert(0, '#')
+    print(formatted_data)
+    return formatted_data
+
+def formatting_xlsx(file_name):
+    wb = openpyxl.load_workbook(file_name)
+    active_sheet = wb.active
+    excel_data = list()
+    for row in active_sheet.iter_rows():
+        row_data = list()
+        for cell in row:
+            row_data.append(str(cell.value))
+        excel_data.append(row_data)
+    formatted_data = excel_data[1::]
+    del formatted_data[0][0]
+    formatted_data[0].insert(0, '#')
+    print(formatted_data)
+    return formatted_data
 
 def Plate_layout(request):
     return render(request, 'Plate_layout.html')
