340f837999c22e5b1074c0cddbe38736a31f6f6e
casper <s1114264@student.hsleiden.nl>
2022-06-15 15:52:49 +0200

    dilution toegevoegd

--- ELISA_app/ELISA_core/views.py
+++ ELISA_app/ELISA_core/views.py
@@ -97,8 +97,107 @@ def database(data_string):
     )
     id += 1
 
+totaal = []
+check = ''
 
 def Plate_layout(request):
+    global check
+    if request.method == 'POST':
+        if request.POST.get('file_submit'):
+            if request.FILES.getlist("my_file") == []:
+                check = 'error'
+                return render(request, 'Plate_layout.html', {
+                    'check': check,
+                })
+            excel_file = request.FILES["my_file"]
+            wb = openpyxl.load_workbook(excel_file)
+            active_sheet = wb.active
+            excel_data = list()
+            for row in active_sheet.iter_rows():
+                row_data = list()
+                for cell in row:
+                    row_data.append(str(cell.value))
+                excel_data.append(row_data)
+            temp = []
+            global totaal
+            counter = 0
+            for i in excel_data:
+                i = [e for e in i if e not in ('None')]
+                if len(i) != 0:
+                    if counter == 1:
+                        i.insert(0, '#')
+                    temp.append(i)
+                    counter += 1
+                    if counter == 10:
+                        totaal.append(temp)
+                        counter = 0
+                        temp = []
+            check = 'go'
+            return render(request, 'Plate_layout.html', {
+                'totaal': totaal,
+                'check': check,
+            })
+        if request.POST.get('standaard_input'):
+            values = request.POST.get('standaard')
+            counter = 0
+            counter2 = 0
+            for i in totaal:
+                for j in i[2:]:
+                    if counter2 != 7:
+                        j[1] = float(values)
+                        j[2] = float(values)
+                        values = float(values)/2
+                    elif counter2 == 7:
+                        j[1] = '#'
+                        j[2] = '#'
+                    counter2 += 1
+                counter += 1
+                values = request.POST.get('standaard')
+                counter2 = 0
+            check = 'go'
+            return render(request, 'Plate_layout.html', {
+            'totaal': totaal,
+            'check': check,
+        })
+    else:
+        return render(request, 'Plate_layout.html', {
+            'totaal': totaal,
+            'check': check,
+        })
+
+end_dilution = []
+def Dilutions(request):
+    global end_dilution
+    if request.method == 'POST':
+        if request.POST.get('dilution_submit'):
+            dilution = request.POST.get('dilution')
+            ll = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+            end_list = [["#", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]]
+            for i in range(9):
+                temp = []
+                for x in range(13):
+                    if i == 0:
+                        if x == 0:
+                            temp.append(ll[i])
+                        elif x == 1 or x == 2 or x == 3 or x == 8:
+                            temp.append("1")
+                        else:
+                            temp.append(dilution)
+                    else:
+                        if x == 0:
+                            temp.append(ll[i])
+                        elif x == 1 or x == 2:
+                            temp.append("1")
+                        else:
+                            temp.append(dilution)
+                end_list.append(temp)
+            end_dilution = end_list
+            return render(request, 'Dilutions.html', {
+                "end_list": end_list
+            })
+    return render(request, 'Dilutions.html')
+
+def Visualize_data(request):
     data = Plates.objects.values()
     dictionary = {}
     teller = 1
@@ -117,16 +216,9 @@ def Plate_layout(request):
         dictionary[teller] = nested
         nested = []
         teller += 1
-    print(dictionary)
-    return render(request, 'Plate_layout.html', {
+    return render(request, 'Visualize_data.html', {
         'dictionary': dictionary,
     })
-
-def Dilutions(request):
-    return render(request, 'Dilutions.html')
-
-def Visualize_data(request):
-    return render(request, 'Visualize_data.html')
 
 def Cut_off(request):
     return render(request, 'Cut_off.html')
