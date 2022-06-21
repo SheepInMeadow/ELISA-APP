02f24052899ad7cdb78dbfe04102cefc19ec52a0
Mila <s1105428@student.hsleiden.nl>
2022-06-21 12:17:49 +0200

    database.py is now universal for Windows users

--- ELISA_app/ELISA_core/static/python/database.py
+++ ELISA_app/ELISA_core/static/python/database.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import sqlite3
+import os
+
+path = os.path.realpath(__file__).rstrip("ELISA_core\static\python\database.py")+"db.sqlite3"
+#C:\Users\Mila\PycharmProjects\BPEXI\ELISA_app\ELISA_core\static\python\database.py
+#C:\Users\Mila\PycharmProjects\BPEXI\ELISA_app\db.sqlite3
 
 plates = pd.DataFrame(columns=['ID', 'Name', 'Data'])
-path = 'C:\\USERS\\caspe\\PycharmProjects\\BPEXI\\ELISA_app\\'
-cxn = sqlite3.connect(path + 'db.sqlite3')
+cxn = sqlite3.connect(path)
 cxn.cursor().executescript('drop table if exists plates')
 plates.to_sql('plates', cxn, index=False)
 cxn.close()
